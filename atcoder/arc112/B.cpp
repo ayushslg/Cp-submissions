68b9ff828b68a1adf40f8e6b7f7391c93197a3cc
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-02-13 18:33:00 +0530

    Add solution for problem `B - -- - B` Link: https://atcoder.jp/contests/arc112/tasks/arc112_b Tags: AtCoder, *400 Ref: https://atcoder.jp/contests/arc112/submissions/20153770

---
+++ atcoder/arc112/B.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("O3")
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>//s.order_of_key(val) *s.find_by_order(ind)
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) { 
+    ll res = 1;    x = x % p;  
+    while(y > 0) {
+        if(y & 1) res = (res*x) % p; 
+        y = y>>1;    x = (x*x) % p; 
+    } return res; 
+}
+inline void solve()
+{
+    
+    ll b,c;
+    cin>>b>>c;
+    /*ll p=c/2;
+    p=p+b;
+    if(b<0)
+    p=p-2*b;
+    cout<<p*2<<endl;*/
+    ll ans=1;
+    
+    if(b==0)
+    {
+        cout<<c;
+        return;
+    }
+    if(c==1)
+    {
+        cout<<2<<endl;
+        return;
+    }
+    if(b<0)
+    {
+        b=b-2*b;
+        c++;
+    }
+        ll p=c-1;
+        ans=2;
+        ans+=p-1;
+        p=c-1;
+        ll q=(b-1)*2 + 1;
+        q=min(q,p);
+        ans+=q;
+        cout<<ans<<endl;
+        
+    
+    
+}
+int main()
+{
+    Imposter
+    
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+        
+    }
+    return 0;
+}
