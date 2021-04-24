1ef0eb3436fa2edc7f58cdab477c3e4b51fdd082
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-04-24 17:34:00 +0530

    Add solution for problem `B - Intersection` Link: https://atcoder.jp/contests/abc199/tasks/abc199_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc199/submissions/21998910

---
+++ atcoder/abc199/B.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#pragma GCC optimize("O3")
+using namespace __gnu_pbds;
+#define ordered_set tree<ll, null_type,less_equal<ll>, rb_tree_tag,tree_order_statistics_node_update>//s.order_of_key(val) *s.find_by_order(ind)
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+const ll MOD=1000000007LL;
+// const ll MOD=998244353LL;
+const ll mod=MOD;
+
+template <int mod>
+struct ModInt {
+    int x;
+    ModInt() : x(0) {}
+    ModInt(long long x_) {
+        if ((x = x_ % mod + mod) >= mod) x -= mod;
+    }
+    ModInt& operator+=(ModInt rhs) {
+        if ((x += rhs.x) >= mod) x -= mod;
+        return *this;
+    }
+    ModInt& operator-=(ModInt rhs) {
+        if ((x -= rhs.x) < 0) x += mod;
+        return *this;
+    }
+    ModInt& operator*=(ModInt rhs) {
+        x = (unsigned long long)x * rhs.x % mod;
+        return *this;
+    }
+    ModInt& operator/=(ModInt rhs) {
+        x = (unsigned long long)x * rhs.inv().x % mod;
+        return *this;
+    }
+ 
+    ModInt operator-() const { return -x < 0 ? mod - x : -x; }
+    ModInt operator+(ModInt rhs) const { return ModInt(*this) += rhs; }
+    ModInt operator-(ModInt rhs) const { return ModInt(*this) -= rhs; }
+    ModInt operator*(ModInt rhs) const { return ModInt(*this) *= rhs; }
+    ModInt operator/(ModInt rhs) const { return ModInt(*this) /= rhs; }
+    bool operator==(ModInt rhs) const { return x == rhs.x; }
+    bool operator!=(ModInt rhs) const { return x != rhs.x; }
+    ModInt inv() const { return pow(*this, mod - 2); }
+ 
+    friend ostream& operator<<(ostream& s, ModInt<mod> a) {
+        s << a.x;
+        return s;
+    }
+    friend istream& operator>>(istream& s, ModInt<mod>& a) {
+        s >> a.x;
+        return s;
+    }
+};
+ 
+using mint = ModInt<MOD>;
+
+inline void solve()
+{
+    ll n;
+    cin>>n;
+    ll ans=0;
+    vll a(n),b(n);
+    input(a,n);
+    input(b,n);
+    for(int i=1;i<=1000;i++)
+    {
+        ll q=0;
+        for(int j=0;j<n;j++)
+        {
+            if(i>=a[j] and i<=b[j])
+            q++;
+        }
+        if(q==n)
+        ans++;
+    }
+    cout<<ans<<endl;
+}
+int main()
+{
+    Imposter
+    
+    int t=1;
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+        
+    }
+    return 0;
+}
