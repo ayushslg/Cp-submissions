226d0e4a5c9abbb34dc4ed7684cddd9b2ae8fd8f
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-04-25 12:09:00 +0530

    Add solution for problem `C - Meme Problem` Link: https://codeforces.com/contest/1076/problem/C Tags: binary search, math, *1300 Ref: https://codeforces.com/contest/1076/submission/114174602

---
+++ codeforces/1076/C.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define ld long double
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) { 
+    ll res = 1;    x = x % p;  
+    while(y > 0) {
+        if(y & 1) res = (res*x) % p; 
+        y = y>>1;    x = (x*x) % p; 
+    } return res; 
+}
+inline void solve()
+{
+    
+    ll d;
+    cout<<fixed<<setprecision(10);
+    cin>>d;
+    if((d*d)<(4*d))
+    {
+        cout<<"N\n";
+        return;
+    }
+    ld z=(ld)((d*d)-(4*d));
+    z=sqrt(z);
+    z=z+(ld)d;
+    z=z/2.0;
+    if(d-z>=0)
+    {
+        cout<<"Y "<<z<<" "<<d-z<<endl;
+    }
+    else
+    {
+        cout<<"N\n";
+    }
+
+}
+int main()
+{
+    Imposter
+    
+    int t=1;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+        
+    }
+    return 0;
+}
