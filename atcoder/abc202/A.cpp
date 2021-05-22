1761ab394b0c343c4cbb8af3765c8803bfc8b38d
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-05-22 17:31:00 +0530

    Add solution for problem `A - Three Dice` Link: https://atcoder.jp/contests/abc202/tasks/abc202_a Tags: AtCoder, *100 Ref: https://atcoder.jp/contests/abc202/submissions/22792967

---
+++ atcoder/abc202/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) {
+    ll res = 1;    x = x % p;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;    x = (x * x) % p;
+    } return res;
+}
+inline void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << 21 - a - b - c << endl;
+
+}
+int main()
+{
+    Imposter
+
+    int t = 1;
+    //cin>>t;
+    while (t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
