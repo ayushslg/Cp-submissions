599a25844a1bfe1724b0856979ae62dc697c5f14
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-06-18 02:21:00 +0530

    Add solution for problem `F - Interesting Function` Link: https://codeforces.com/contest/1538/problem/F Tags: binary search, dp, math, number theory, *1500 Ref: https://codeforces.com/contest/1538/submission/119746725

---
+++ codeforces/1538/F.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) {
+    ll res = 1;    x = x % p;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;    x = (x * x) % p;
+    } return res;
+}
+inline void solve()
+{
+    ll l, r;
+    cin >> l >> r;
+    function<int(int)> fnd = [](ll n)
+    {
+        ll ans = 0, p = 1;
+        for (int i = 1; i <= 10; i++)
+        {
+            ans += (n / p);
+            //cout << ans << endl;
+            p = p * 10;
+        }
+        return ans;
+    };
+    cout << fnd(r) - fnd(l) << endl;
+
+}
+int main()
+{
+    Imposter
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
