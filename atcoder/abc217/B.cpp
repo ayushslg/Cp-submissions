e79e9f5566b48782fd247152ce2013532a420126
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-09-04 17:33:00 +0530

    Add solution for problem `B - AtCoder Quiz` Link: https://atcoder.jp/contests/abc217/tasks/abc217_b Tags: AtCoder, *200 Ref: https://atcoder.jp/contests/abc217/submissions/25561654

---
+++ atcoder/abc217/B.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) {
+    ll res = 1;    x = x % p;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;    x = (x * x) % p;
+    } return res;
+}
+inline void solve()
+{
+    map<string, int>mp;
+    mp["ABC"] = 1;
+    mp["ARC"] = 1;
+    mp["AGC"] = 1;
+    mp["AHC"] = 1;
+    string s;
+    for (int i = 0; i < 3; i++)
+    {
+        cin >> s;
+        mp[s]--;
+    }
+    for (auto it : mp)
+    {
+        if (it.second == 1)
+        {
+            cout << it.first << endl;
+        }
+    }
+}
+int main()
+{
+    Imposter
+
+    int t = 1;
+    // cin>>t;
+    while (t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
