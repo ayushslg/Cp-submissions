aeac26072695b301d8e286a557ccd8e4375fab04
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2021-10-28 20:08:00 +0530

    Add solution for problem `C - Array Elimination` Link: https://codeforces.com/contest/1602/problem/C Tags: bitmasks, math, number theory, *1300 Ref: https://codeforces.com/contest/1602/submission/133365931

---
+++ codeforces/1602/C.cpp
@@ -0,0 +1,62 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define f(n) for(ll i=0;i<n;i++)
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) {
+    ll res = 1;    x = x % p;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;    x = (x * x) % p;
+    } return res;
+}
+bool check(vector<int>&a, vector<int>&prev)
+{
+    for (int i = 0; i < a.size(); i++)
+        if (a[i] != prev[i])
+            return false;
+    return true;
+}
+inline void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    int gec = 0;
+    for (int bit = 0; bit < 31; bit++)
+    {
+        int q = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if ((1ll << bit)&a[i])
+                q++;
+        }
+        gec = __gcd(q, gec);
+    }
+    for (int i = 1; i <= n; i++)
+        if (!(gec % i))
+            cout << i << " ";
+    cout << endl;
+}
+int main()
+{
+    Imposter
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
