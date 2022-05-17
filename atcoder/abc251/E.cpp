d5b8d145dd4882cccea4d62c8b754aca7740bec1
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2022-05-17 18:52:00 +0530

    Add solution for problem `E - Takahashi and Animals` Link: https://atcoder.jp/contests/abc251/tasks/abc251_e Tags: AtCoder, *500 Ref: https://atcoder.jp/contests/abc251/submissions/31767172

--- atcoder/abc251/E.cpp
+++ atcoder/abc251/E.cpp
@@ -28,13 +28,7 @@ inline void solve()
 
     ll ans1 = 0;
 
-    if (n == 2)
-    {
-        cout << min(a[1], a[2]);
-        return;
-    }
-
-    vector<ll>dp(n + 2, 0);
+    vector<ll>dp(n + 1, 0);
 
     dp[1] = a[1];
     dp[2] = a[1];
@@ -50,14 +44,12 @@ inline void solve()
 
     a[0] = a[n];
 
-    dp[2] = dp[3] = a[2];
-
-    for (int i = 4; i <= (n - 1); i++)
+    for (int i = 2; i <= (n - 1); i++)
     {
-        dp[i] = min(dp[i - 1] + a[i], dp[i - 2] + a[i - 1]);
+        dp[i ] = min(dp[i - 1] + a[i], dp[i - 2] + a[i - 1]);
     }
 
-    ans1 = min(ans1, dp[n - 1] + a[n]);
+    ans1 = min(ans1, dp[n - 1]);
 
     cout << ans1 << endl;
 }
