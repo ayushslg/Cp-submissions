9e78c063eecd3a283d805394271a07d4484da5b2
Ayush Tiwari <ayushtiwari.slg01@gmail.com>
2022-06-18 21:15:00 +0530

    Add solution for problem `E - By Elevator or Stairs?` Link: https://codeforces.com/contest/1249/problem/E Tags: dp, shortest paths, *1700 Ref: https://codeforces.com/contest/1249/submission/161094837

---
+++ codeforces/1249/E.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+#define Imposter ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define pb push_back
+#define ll long long int
+using namespace std;
+#define rep(i,a,b) for(ll i=a;i<b;i++)
+#define input(arr,n) for(ll i1=0;i1<n;i1++ )cin>>arr[i1]
+#define endl "\n"
+#define vll vector<ll>
+#define vlll vector<vll>
+#define all(x) x.begin(),x.end()
+ll power(ll x, ll y, ll p) {
+    ll res = 1;    x = x % p;
+    while (y > 0) {
+        if (y & 1) res = (res * x) % p;
+        y = y >> 1;    x = (x * x) % p;
+    } return res;
+}
+int f(int ind, int n, int c, vector<int>& stair, vector<int>& lift, int state, vector<vector<int>>& dp)
+{
+    if (ind == 0)
+    {
+        if (state)
+            return c;
+        return 0;
+    }
+    if (dp[ind][state] != INT_MAX)
+        return dp[ind][state];
+    int st = 1e8;
+    if (state != 1)
+        st = stair[ind - 1] + f(ind - 1, n, c, stair, lift, 0, dp);
+    int lft = 1e8;
+    lft = min(c + lift[ind - 1] + f(ind - 1, n, c, stair, lift, 0, dp), lift[ind - 1] + f(ind - 1, n, c, stair, lift, 1, dp));
+    return dp[ind][state] = min(lft, st);
+
+}
+inline void solve()
+{
+
+    int n;
+    cin >> n;
+    int c;
+    cin >> c;
+    vector<int> stair(n - 1);
+    vector<int> lift(n - 1);
+    input(stair, n - 1);
+    input(lift, n - 1);
+    vector<int>curr(2, 0), prev(2, 0);
+    prev[1] = c;
+    cout << 0 << " ";
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int state = 0; state < 2; state++)
+        {
+            int st = 1e8;
+            if (state != 1)
+                st = stair[ind - 1] + prev[state];
+            int lft = 1e8;
+            lft = min(c + lift[ind - 1] + prev[0], lift[ind - 1] + prev[1] );
+            curr[state] = min(lft, st);
+        }
+        cout << min(curr[0], curr[1]) << " ";
+        prev = curr;
+    }
+}
+int main()
+{
+    Imposter
+
+    int t = 1;
+    //cin >> t;
+    while (t--)
+    {
+        solve();
+
+    }
+    return 0;
+}
